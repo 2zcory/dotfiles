c8bf761ec21a65e56ce2762f6dae95156472a51e
TriNK「コリー」 <khanhtri009@gmail.com>
2025-05-25 08:41:28 +0700

    2025/05/25 08:41

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,14 +4,38 @@
 
 local map = vim.keymap.set
 
+---@class KeymapOpts
+---@field noremap? boolean
+---@field silent? boolean
+---@field expr? boolean
+---@field nowait? boolean
+---@field desc? string
+---@field buffer? number|boolean
+---@param opts KeymapOpts
+local function keymap_opts(opts)
+  return vim.tbl_extend("force", {
+    noremap = true,
+    silent = true,
+  }, opts or {})
+end
+
 -- Tab
 
 -- cmd
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
 -- Windows
-map("n", "<leader>\\", "<cmd>vsplit<CR>", { desc = "Windows - split window right" })
+map("n", "<leader>\\", "<cmd>vsplit<CR>", keymap_opts({ desc = "Windows - split window right" }))
+map("n", "<M-Up>", "<cmd>resize +2<CR>", keymap_opts({ desc = "Alt + Down: Increase height" }))
+map("n", "<M-Down>", "<cmd>resize -2<CR>", keymap_opts({ desc = "Alt + Down: Increase height" }))
+map("n", "<M-Left>", "<cmd>vertical resize -4<CR>", keymap_opts({ desc = "Alt + Down: Increase height" }))
+map("n", "<M-Right>", "<cmd>vertical resize +4<CR>", keymap_opts({ desc = "Alt + Down: Increase height" }))
 
 -- Telescope
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live Grep - Search Words in Workspace" })
+map(
+  "n",
+  "<leader>fw",
+  "<cmd>Telescope live_grep<CR>",
+  keymap_opts({ desc = "Telescope Live Grep - Search Words in Workspace" })
+)
 
